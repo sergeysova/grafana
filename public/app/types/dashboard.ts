da4ef83f0506d5d901142217de229fbc4aee2dae
Ivan Ortega Alba <ivanortegaalba@gmail.com>
2022-07-19 17:05:51 +0200

    chore: Remove unnecesary space

--- public/app/types/dashboard.ts
+++ public/app/types/dashboard.ts
@@ -42,7 +42,6 @@ export interface DashboardMeta {
   hasUnsavedFolderChange?: boolean;
   annotationsPermissions?: AnnotationsPermissions;
   publicDashboardAccessToken?: string;
-
 }
 
 export interface AnnotationActions {
